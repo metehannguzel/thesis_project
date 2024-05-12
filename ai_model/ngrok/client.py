97baac9d84e0cf3519fb8b4e6a577886b2c0a986
metehan <66705106+metehannguzel@users.noreply.github.com>
2024-05-12 21:51:32 +0300

    Update client.py

--- ai_model/ngrok/client.py
+++ ai_model/ngrok/client.py
@@ -1,14 +1,4 @@
 import requests
 
-response = requests.get('ngrok url')
+response = requests.get("ngrok url")
 print(response.text)
-
-####################################################################################################
-
-"""import socket
-
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect("ngrok url")
-
-print(client.recv(1024).decode())
-client.send("Client is running!".encode())"""
