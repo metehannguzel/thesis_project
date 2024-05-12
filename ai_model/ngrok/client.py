b7763de15c20cbef9a6b9749e55db0d9a0d2d577
metehan <66705106+metehannguzel@users.noreply.github.com>
2024-05-12 19:02:52 +0300

    Add files via upload

---
+++ ai_model/ngrok/client.py
@@ -0,0 +1,14 @@
+import requests
+
+response = requests.get('https://bbf0-31-223-46-159.ngrok-free.app')
+print(response.text)
+
+####################################################################################################
+
+"""import socket
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect("68a9-31-223-46-159.ngrok-free.app")
+
+print(client.recv(1024).decode())
+client.send("Client is running!".encode())"""
---
+++ ai_model/ngrok/server.py
@@ -0,0 +1,55 @@
+import socket
+import subprocess
+import os
+import json
+
+model_script_path = os.path.join(os.getcwd(), "main.py")
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(("localhost", 9999))
+
+server.listen()
+
+while True:
+    client, addr = server.accept()
+    client.send("HTTP/1.1 200 OK\nContent-Type: application/json\n\n".encode())
+    result = subprocess.run(["python", model_script_path], capture_output=True, text=True)
+
+    if result.returncode == 0 and result.stdout:
+        stdout_data = result.stdout
+        start_index = stdout_data.rfind('{')
+        end_index = stdout_data.rfind('}') + 1
+
+        if start_index != -1 and end_index != -1:
+            json_string = stdout_data[start_index:end_index]
+            try:
+                response_data = json.loads(json_string)
+                response = json.dumps(response_data)
+                print(response)
+                client.send(response.encode())
+            except json.JSONDecodeError:
+                client.send("Error decoding JSON output.".encode())
+        else:
+            client.send("No valid JSON data found in script output.".encode())
+    else:
+        client.send(f"Error: {result.stderr}".encode())
+
+    client.close()
+
+# CMD
+# 
+# cd C:\Users\guzel\OneDrive\Documents\thesis\thesis_project\ngrok
+# 
+# ngrok.exe http 9999
+# 
+# ngrok.exe tcp 9999
+
+# ----------------------------------------------------------------------------------------------------
+
+# ANACONDA PROMPT
+# 
+# conda activate pt_gpu
+# 
+# cd C:\Users\guzel\OneDrive\Documents\thesis\thesis_project\ngrok
+# 
+# python -m http.server 9999
